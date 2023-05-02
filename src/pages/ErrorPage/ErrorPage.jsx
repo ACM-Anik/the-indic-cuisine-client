aa0baebe8aa966476e7e4c6c28ca27553171b5d4
ACM-Anik <anikmojumder5252@gmail.com>
2023-05-02 17:03:05 +0600

    added a image in error page and chefs Reciepes created

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -7,7 +7,7 @@ const ErrorPage = () => {
     const { error, status } = useRouteError()
 
     return (
-        <div className="hero min-h-screen " style={{ backgroundImage: `url("../../assets/images/indian-cuisine.jpg")` }}>
+        <div className="hero min-h-screen" style={{ backgroundImage: `url("https://img.freepik.com/free-photo/top-view-sari-indian-food_23-2148747657.jpg?t=st=1682980333~exp=1682980933~hmac=8ab36bdd53b01f366b095c6a65384cff20dff2cdd5d5f055b86e5c6f42e09b6a")` }}>
     <div className="hero-overlay bg-opacity-60"></div>
     <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md flex flex-col items-center justify-center px-5 my-8">
